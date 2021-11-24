c8b8287da0bc04ae05f9820e0c2c2e62df01c39f
muttta <93360763+muttta@users.noreply.github.com>
2021-11-24 18:38:01 +0900

    Userモデルへバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,10 @@ class User < ApplicationRecord
   has_many :records
 
   validates :nickname,        presence: true
-  validates :email,           presence: true, uniqueness: true, format: {with: (/\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z/i)}
-  validates :encrypted_password, presence: true, format: {with: (/\A[a-z0-9]{6,}\z/i)}
   validates :last_name,       presence: true, format: {with: (/\A[ぁ-んァ-ヶー-龥々]+\z/)}
   validates :first_name,      presence: true, format: {with: (/\A[ぁ-んァ-ヶー-龥々]+\z/)}
   validates :last_name_kana,  presence: true, format: {with: (/\A[ァ-ヶーー]+\z/)}
   validates :first_name_kana, presence: true, format: {with: (/\A[ァ-ヶーー]+\z/)}
   validates :birth,           presence: true
-
+  
 end
