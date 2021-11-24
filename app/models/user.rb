b233151679398baee75a35d1321832e6ffbcdc69
muttta <93360763+muttta@users.noreply.github.com>
2021-11-24 16:26:56 +0900

    devise関連viewファイル生成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,13 @@ class User < ApplicationRecord
   has_many :items
   has_many :records
 
-  validetes :nickname,        presence: true
-  validetes :email,           presence: true, uniqeness: true, format: {with: (/\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z/i)}
-  validetes :encrypted_password, presence: true, format: {with: (/\A[a-z0-9]{6,}\z/i)}
-  validetes :last_name,       presence: true, format: {with: (/\A[ぁ-んァ-ヶー-龥々]+\z/)}
-  validetes :first_name,      presence: true, format: {with: (/\A[ぁ-んァ-ヶー-龥々]+\z/)}
-  validetes :last_name_kana,  presence: true, format: {with: (/\A[ァ-ヶーー]+\z/)}
-  validetes :first_name_kana, presence: true, format: {with: (/\A[ァ-ヶーー]+\z/)}
-  validetes :birth,           presence: true
+  validates :nickname,        presence: true
+  validates :email,           presence: true, uniqueness: true, format: {with: (/\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z/i)}
+  validates :encrypted_password, presence: true, format: {with: (/\A[a-z0-9]{6,}\z/i)}
+  validates :last_name,       presence: true, format: {with: (/\A[ぁ-んァ-ヶー-龥々]+\z/)}
+  validates :first_name,      presence: true, format: {with: (/\A[ぁ-んァ-ヶー-龥々]+\z/)}
+  validates :last_name_kana,  presence: true, format: {with: (/\A[ァ-ヶーー]+\z/)}
+  validates :first_name_kana, presence: true, format: {with: (/\A[ァ-ヶーー]+\z/)}
+  validates :birth,           presence: true
 
 end
