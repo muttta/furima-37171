b20e6a592903d96adf6b7902083582aa988a8faa
muttta <93360763+muttta@users.noreply.github.com>
2021-11-24 18:35:56 +0900

    usersテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_11_24_025540) do
     t.string "nickname", null: false
     t.string "last_name", null: false
     t.string "first_name", null: false
-    t.string "last_name_kane", null: false
+    t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
