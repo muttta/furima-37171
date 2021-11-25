5211c872f41d6ad3625a055629d7159687c0a953
muttta <93360763+muttta@users.noreply.github.com>
2021-11-25 19:43:49 +0900

    Itemモデルへバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
-  
+  belongs_to :user
+  has_one    :record
+
+  validates :item_name            presence: true
+  validates :item_price           presence: true
+  validates :description          presence: true
+  validates :category_id          presence: true
+  validates :status_id            presence: true
+  validates :delivery_period_id   presence: true
+  validates :delivery_fee_id      presence: true
+  validates :shipment_source_id   presence: true
+
 end
