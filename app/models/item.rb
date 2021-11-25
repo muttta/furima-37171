299314a88da4e56118d05fb9ae64412a03dd41dc
muttta <93360763+muttta@users.noreply.github.com>
2021-11-25 23:47:53 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,18 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  has_one    :record
-
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to       :category
+  belongs_to       :delivery_period
+  belongs_to       :delivery_fee
+  belongs_to       :status
+  belongs_to       :shipment_source
+  belongs_to       :user
+  has_one          :record
   has_one_attached :image
-  
-  validates :item_name            presence: true
-  validates :item_price           presence: true
-  validates :description          presence: true
-  validates :category_id          presence: true
-  validates :status_id            presence: true
-  validates :delivery_period_id   presence: true
-  validates :delivery_fee_id      presence: true
-  validates :shipment_source_id   presence: true
+
+  validates :item_name, :item_price, :description, :category_id, :status_id, :delivery_period_id, :delivery_fee_id, presence: true
+  validates :category_id, :status_id, :delivery_period_id, :delivery_fee_id, numericality: {other_than: 1, message: "can't be blank"}
+
+  validates :item_price            
+  validates :description          
 
 end
