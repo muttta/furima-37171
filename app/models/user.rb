1e0d8d3adaf521d54cb11e0da66d6d45ef8743bd
muttta <93360763+muttta@users.noreply.github.com>
2021-11-27 14:13:55 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,11 @@ class User < ApplicationRecord
   #has_many :records
 
   validates :nickname,        presence: true
-  validates :last_name,       presence: true, format: {with: (/\A[ぁ-んァ-ヶー-龥々ー]+\z/), message: "は全角(漢字・ひらがな・カタカナ)で入力してください"}
-  validates :first_name,      presence: true, format: {with: (/\A[ぁ-んァ-ヶー-龥々ー]+\z/), message: "は全角(漢字・ひらがな・カタカナ)で入力してください"}
-  validates :last_name_kana,  presence: true, format: {with: (/\A[ァ-ヶーー]+\z/), message: "は全角(カタカナ)で入力してください"}
-  validates :first_name_kana, presence: true, format: {with: (/\A[ァ-ヶーー]+\z/), message: "は全角(カタカナ)で入力してください"}
+  validates :last_name,       presence: true, format: { with: (/\A[ぁ-んァ-ヶー-龥々ー]+\z/), message: "は全角(漢字・ひらがな・カタカナ)で入力してください" }
+  validates :first_name,      presence: true, format: { with: (/\A[ぁ-んァ-ヶー-龥々ー]+\z/), message: "は全角(漢字・ひらがな・カタカナ)で入力してください" }
+  validates :last_name_kana,  presence: true, format: { with: (/\A[ァ-ヶーー]+\z/), message: "は全角(カタカナ)で入力してください" }
+  validates :first_name_kana, presence: true, format: { with: (/\A[ァ-ヶーー]+\z/), message: "は全角(カタカナ)で入力してください" }
   validates :birth,           presence: true
-  validates :password, :password_confirmation, format: {with: (/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i), message: "は半角英数字混合で入力してください"}
+  validates :password, :password_confirmation, format: { with: (/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i), message: "は半角英数字混合で入力してください" }
   
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    item_name    {"test"}
-    item_price   {1000}
-    description  {"test"}
-    category_id        {2}
-    status_id          {2}
-    delivery_fee_id    {2}
-    delivery_period_id {2}
-    shipment_source_id {2}
+    item_name    { "test" }
+    item_price   { 1000 }
+    description  { "test" } 
+    category_id        { 2 }
+    status_id          { 2 }
+    delivery_fee_id    { 2 }
+    delivery_period_id { 2 }
+    shipment_source_id { 2 }
 
     association :user
 
