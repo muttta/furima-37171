9b5b215e6594a258e5f5922beb1e6734aa252e83
muttta <93360763+muttta@users.noreply.github.com>
2021-11-29 15:51:20 +0900

    購入機能に必要なモデルを作成

--- app/models/user_payment.rb
+++ app/models/user_payment.rb
@@ -6,7 +6,7 @@ class UserPayment
     validates :postal_code format: {with: (/\A\d{3}[-]\d{4}\z/)}
     validates :phone       format: {with: (/\A\d{10,11}\z/)}
     def save
-      Recrd.create(user_id: user_id, item_id: item_id)
+      Record.create(user_id: user_id, item_id: item_id)
       Address.create(postal_code: postal_code, state_id: state_id, city: city, address: address, building: building, phone: phone, record_id: record_id)
     end
   end
