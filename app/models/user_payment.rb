c6453d9dbafbce70bbaf9a21cc982ac3a27d1c5c
muttta <93360763+muttta@users.noreply.github.com>
2021-11-30 09:28:23 +0900

    バリデーションの修正

--- app/models/user_payment.rb
+++ app/models/user_payment.rb
@@ -3,10 +3,10 @@ class UserPayment
     attr_accessor :user_id, :item_id, :postal_code, :shipment_source_id, :city, :address, :building, :phone, :record_id
 
     validates :user_id, :item_id, :postal_code, :shipment_source_id, :city, :address, :phone, presence: true
-    validates :postal_code, format: {with: (/\A\d{3}[-]\d{4}\z/)}
-    validates :phone,       format: {with: (/\A\d{10,11}\z/)}
+    validates :postal_code, format: { with: (/\A[0-9]{3}-[0-9]{4}\z/)}
+    validates :phone,       format: { with: (/\A[0-9]{10,11}\z/)}
     def save
-      Record.create(user_id: user_id, item_id: item_id)
-      Address.create(postal_code: postal_code, shipment_source_id: shipment_source_id, city: city, address: address, building: building, phone: phone, record_id: record_id)
+      record = Record.create(user_id: user_id, item_id: item_id)
+      Address.create(postal_code: postal_code, shipment_source_id: shipment_source_id, city: city, address: address, building: building, phone: phone, record_id: record.id)
     end
   end
