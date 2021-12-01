f73178b75fcdef44d7fff241819efca909a3070f
muttta <93360763+muttta@users.noreply.github.com>
2021-12-01 12:52:29 +0900

    非公開情報を環境変数へ設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,6 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_789cd3ffbb118a01ebf40492")
-  const submit = document.getElementById("button")
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
+  const submit = document.getElementById("button") 
   submit.addEventListener("click",(e) => {
     e.preventDefault();
     const formResult = document.getElementById('charge-form')
@@ -12,17 +12,19 @@ const pay = () => {
       exp_month: formData.get("user_payment[exp_month]"),
       exp_year: `20${formData.get("user_payment[exp_year]")}`,
     };
-    Payjp.createToken(card, (status,response) => {
+    Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
-        const renderDom = document.getElementById("chharge-form");
-        const tokenObj = `<input value=${token} name='tokenObj' type="hidden">`;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
-      document.getElementById("card_number").removeAttribute("name");
-      document.getElementById("card_exp_month").removeAttribute("name");
-      document.getElementById("card_exp_year").removeAttribute("name");
-      document.getElementById("card_cvc").removeAttribute("name");
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
     })
   });
 };
